b95bdcbb69a32cedc9a235c1d800b39bb35335eb
Nish <nishanthchandradas@gmail.com>
2019-10-24 16:02:11 +0100

    terminaltables and enquirer added

--- masts.py
+++ masts.py
@@ -1,16 +1,52 @@
 import csv
+from pprint import pprint
+
+import inquirer
+from terminaltables import AsciiTable
+
 
 def csv_to_dict():
     csv_dictionary = csv.DictReader(open("data/mobile_phone_masts.csv"))
-    return csv_dictionary
+    return csv_dictionary, csv_dictionary.fieldnames
 
 
-def ascending_by_current_rent(mast_data):
-    csv_dict = sorted(mast_data, key=lambda row: float(row['Current Rent']))
-    for row in csv_dict:
-        print(row)
+def ascending_by_current_rent(columns, mast_data):
+    sorted_data = sorted(mast_data, key=lambda row: float(row['Current Rent']))[:5]
+    sorted_data = list(sorted_data)
+    display_table(columns, sorted_data)
 
 
-# mast_data = csv_to_dict()
-#
-# print(ascending_by_current_rent(mast_data))
+def display_table(columns, sorted_data):
+    data_list = []
+    for ordered_dictionary in sorted_data:
+        row_list = []
+        for key, value in ordered_dictionary.items():
+            row_list.append(value)
+        data_list.append(row_list)
+    table_data = [columns]
+    table_data.extend(data_list)
+    table = AsciiTable(table_data)
+    print(table.table)
+
+
+def execute_operation(answers):
+    mast_data, columns = csv_to_dict()
+    if answers["operation"] == "First 5 masts sorted by Current Rent in ascending order":
+        ascending_by_current_rent(columns, mast_data)
+
+
+questions = [
+    inquirer.List('operation',
+                  message="Which operation would you like to run?",
+                  choices=[
+                      'First 5 masts sorted by Current Rent in ascending order',
+                      'Large',
+                      'Standard',
+                      'Medium',
+                      'Small',
+                      'Micro'
+                  ],
+                  ),
+]
+answers = inquirer.prompt(questions)
+execute_operation(answers)
