4586111b2ffeec7d95a993b2ededf0940c72999f
Nish chandradas <nishanthchandradas@gmail.com>
2019-10-26 23:29:31 +0100

    complete

--- masts.py
+++ masts.py
@@ -11,46 +11,56 @@ from pprint import pprint
 import inquirer
 from terminaltables import AsciiTable
 
+masts_data_csv = "data/mobile_phone_masts.csv"
 
-def csv_to_dict():
-    csv_dictionary = csv.DictReader(open("data/mobile_phone_masts.csv"))
+
+def csv_to_dict(masts_data_csv):
+    csv_dictionary = csv.DictReader(open(masts_data_csv))
     return csv_dictionary, csv_dictionary.fieldnames
 
 
 def ascending_by_current_rent(columns, mast_data):
     sorted_data = list(sorted(mast_data, key=lambda row: float(row['Current Rent']))[:5])
-    display_table(columns, sorted_data)
+    tables = []
+    tables.append(display_table(columns, sorted_data))
+    return sorted_data, [display_table(columns, sorted_data)]
 
 
 def specific_lease_years(columns, mast_data):
     mast_data = list(mast_data)
     sorted_data = []
+    tables = []
     for n, row in enumerate(mast_data):
         try:
             if int(row["Lease Years"]) == 25:
                 sorted_data.append(row)
         except ValueError:
             pass
-    display_table(columns, sorted_data)
+    tables.append(display_table(columns, sorted_data))
     # List Comprehension
     total_rent = sum([(int(item["Current Rent"])) for item in sorted_data])
-    display_table(["Total Rent"], [[total_rent]])
+    tables.append(display_table(["Total Rent"], [[total_rent]]))
+    return sorted_data, total_rent, [tables]
+
 
 def tenant_mast_count(mast_data):
     mast_data = list(mast_data)
+    tables = []
     keyfunc = lambda row: row['Tenant Name']
     groups_dict = it.groupby(sorted(mast_data, key=keyfunc), keyfunc)
     grouped_dict = {k: sum(1 for a in g) for k, g in groups_dict}
     groups_list = it.groupby(sorted(mast_data, key=keyfunc), keyfunc)
     grouped_list = [[k, sum(1 for a in g)] for k, g in groups_list]
-    pprint(grouped_dict)
+    dictionaries = [grouped_dict]
     columns = ["Tenant Name", "Mast Count"]
-    display_table(columns, sorted_data=grouped_list)
+    tables.append(display_table(columns, sorted_data=grouped_list))
+    return dictionaries, tables
 
 
 def leases_between_dates(columns, mast_data):
     mast_data = list(mast_data)
     sorted_data = []
+    tables = []
     search_start_date_begin = date(1999, 6, 1)
     search_start_date_end = date(2007, 8, 31)
 
@@ -67,28 +77,32 @@ def leases_between_dates(columns, mast_data):
                 sorted_data.append(row)
         except ValueError:
             pass
-    display_table(columns, sorted_data)
+    tables.append(display_table(columns, sorted_data))
+    return sorted_data, tables
 
 
 def display_table(columns, sorted_data):
     data_list = []
-    if not isinstance(sorted_data[0], list):
-        for ordered_dictionary in sorted_data:
-            row_list = []
-            for key, value in ordered_dictionary.items():
-                row_list.append(value)
-            data_list.append(row_list)
-    table_data = [columns]
-    if not isinstance(sorted_data[0], list):
-        table_data.extend(data_list)
+    if not sorted_data:
+        print("No Results")
     else:
-        table_data.extend(sorted_data)
-    for row in table_data:
-        for n, cell in enumerate(row):
-            if isinstance(cell, str) and not isinstance(sorted_data[0], list):
-                row[n] = textwrap.shorten(cell, width=22, placeholder="...")
-    table = AsciiTable(table_data)
-    print(table.table)
+        if not isinstance(sorted_data[0], list):
+            for ordered_dictionary in sorted_data:
+                row_list = []
+                for key, value in ordered_dictionary.items():
+                    row_list.append(value)
+                data_list.append(row_list)
+        table_data = [columns]
+        if not isinstance(sorted_data[0], list):
+            table_data.extend(data_list)
+        else:
+            table_data.extend(sorted_data)
+        for row in table_data:
+            for n, cell in enumerate(row):
+                if isinstance(cell, str) and not isinstance(sorted_data[0], list):
+                    row[n] = textwrap.shorten(cell, width=22, placeholder="...")
+        table = AsciiTable(table_data)
+        print(table.table)
 
 
 def ask_question():
@@ -105,25 +119,40 @@ def ask_question():
     ]
     if sys.stdout.isatty():
         answers = inquirer.prompt(questions)
-        os.system('cls' if os.name == 'nt' else 'clear')
+        # os.system('cls' if os.name == 'nt' else 'clear')
 
         execute_operation(answers)
     else:
         print("Please run this via a terminal")
 
 
-
 def execute_operation(answers):
-    mast_data, columns = csv_to_dict()
-    if answers["operation"] == "First 5 masts sorted by Current Rent in ascending order":
-        ascending_by_current_rent(columns, mast_data)
-    elif answers["operation"] == "Masts with a 25 year lease and their total rent":
-        specific_lease_years(columns, mast_data)
-    elif answers["operation"] == "A dictionary of all tenants with a count of masts they own":
-        tenant_mast_count(mast_data)
-    elif answers["operation"] == "Leases starting between 1st of June 1999 and 31st August 2007":
-        leases_between_dates(columns, mast_data)
-    ask_question()
+    mast_data, columns = csv_to_dict(masts_data_csv)
+
+    try:
+
+        if answers["operation"] == "First 5 masts sorted by Current Rent in ascending order":
+            sorted_data, tables = ascending_by_current_rent(columns, mast_data)
+        elif answers["operation"] == "Masts with a 25 year lease and their total rent":
+            sorted_data, total_rent, tables = specific_lease_years(columns, mast_data)
+        elif answers["operation"] == "A dictionary of all tenants with a count of masts they own":
+            dictionaries, tables = tenant_mast_count(mast_data)
+        elif answers["operation"] == "Leases starting between 1st of June 1999 and 31st August 2007":
+            sorted_data, tables = leases_between_dates(columns, mast_data)
+
+        try:
+            for dict in dictionaries:
+                pprint(dict)
+        except NameError:
+            pass
+
+        for table in tables:
+            table
+
+        ask_question()
+    except TypeError:
+        # fail graceful
+        pass
 
 
 ask_question()
---
+++ tests/test_masts.py
@@ -0,0 +1,33 @@
+from masts import csv_to_dict, ascending_by_current_rent, specific_lease_years, tenant_mast_count, leases_between_dates
+
+masts_data_csv = "tests/test.csv"
+
+
+def test_ascending_by_current_rent():
+    mast_data, columns = csv_to_dict(masts_data_csv)
+    sorted_data, tables = ascending_by_current_rent(columns, mast_data)
+    ordered_rent = []
+    for row in sorted_data:
+        ordered_rent.append(float(row["Current Rent"]))
+    assert ordered_rent == [0.0, 1.0, 2.0, 3.0, 4.0]
+
+
+def test_specific_lease_years():
+    mast_data, columns = csv_to_dict(masts_data_csv)
+    sorted_data, total_rent, tables = specific_lease_years(columns, mast_data)
+    specified_lease_masts = []
+    for row in sorted_data:
+        specified_lease_masts.append((int(row["Units Reference"])))
+    assert specified_lease_masts == [6, 10]
+
+
+def test_tenant_mast_count():
+    mast_data, columns = csv_to_dict(masts_data_csv)
+    dictionaries, tables = tenant_mast_count(mast_data)
+    assert dictionaries[0]["CTIL (Vodafone/O2/T Mobile) Cornerstone Telecommunications Infrastructure Ltd"] == 5
+
+def test_leases_between_dates():
+    mast_data, columns = csv_to_dict(masts_data_csv)
+    sorted_data, tables = leases_between_dates(columns, mast_data)
+    assert int(sorted_data[0]["Units Reference"]) == 10
+
